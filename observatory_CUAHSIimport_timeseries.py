08118052d839ec5873e8b3cdfa4451774cf9f688
Christina Bandaragoda <cband@uw.edu>
2017-03-14 17:54:51 -0700

    Add files via upload
    I added a VIC command to get Ronda's 2013 data. 
    It's in the pypeline.scripts file.

---
+++ observatory_CUAHSIimport_timeseries.py
@@ -0,0 +1,9 @@
+Convert Emilio's notebook into a .py for grabbing data from CUAHSHI.
+GENERIC.
+Input = bbox,wsdl_url,site_code,  variable_code,
+
+
+See https://ulmo.readthedocs.io/en/latest/api.html#module-ulmo.cuahsi.his_central
+    
+    def 
+    ulmo.cuahsi.wof.get_values(wsdl_url, site_code, variable_code, start=None, end=None, suds_cache=('default', ))
